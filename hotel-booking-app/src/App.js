89e18872718695e5dc6db3a2ec7f81d0ae4668cd
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-12-02 22:03:27 +0530

    Learned about thinking in react

--- hotel-booking-app/src/App.js
+++ hotel-booking-app/src/App.js
@@ -1,27 +1,42 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 
-const Person = (props) => {
+// const API_KEY = "www.omdbapi.com/?apikey=c4b00c87";
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {"  "}Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+function FilterableProductTable({ products }) {
   return (
     <div>
-      <h1> Name: {props.name}</h1>
-      <p> I am a person</p>
-      <p> Age: {props.age}</p>
-      <p>
-        {" "}
-        I have injected another functional component into other component!!
-      </p>
+      <SearchBar />
+      <ProductTable products={products} />
     </div>
   );
-};
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <button>-</button>
-      <h1>{counter}</h1>
-      <button>+</button>
+      <FilterableProductTable products={PRODUCTS} />
     </div>
   );
 };
